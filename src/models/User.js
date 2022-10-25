72a00baa09587d153fc7e671d2e16d7c996ac446
Rodrigo Sena <dias.rodrigosena@gmail.com>
2022-10-25 16:35:52 -0300

    Req 02

---
+++ src/models/User.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+      id: { type: DataTypes.INTEGER, primaryKey: true },
+      displayName: DataTypes.STRING,
+      street: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      image: DataTypes.STRING,
+    },
+    {
+      timestamps: false,
+      tableName: 'users',
+      underscored: true,
+    });
+  
+    return User;
+  };
